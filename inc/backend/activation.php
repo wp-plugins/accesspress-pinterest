0ca6c0e6af093997ba9508b6f25805033db3f74f
Access Keys <Access Keys@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-17 11:34:33 +0000

    * Demo link url change.
    git-svn-id: https://plugins.svn.wordpress.org/accesspress-pinterest/tags/1.1.9@1222666 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/backend/activation.php
@@ -0,0 +1,13 @@
+<?php defined( 'ABSPATH' ) or die( 'No script kiddies please!' ); ?>
+<?php
+$apsp_settings = array();
+$apsp_settings['pinit_js_disable'] = 'off';
+$apsp_settings['js_enabled']='off';
+$apsp_settings['size']='small';
+$apsp_settings['shape']='rectangular';
+$apsp_settings['color']='gray';
+$apsp_settings['language']='eng';
+
+//update of a option table
+update_option( APSP_SETTINGS, $apsp_settings);
+?>
